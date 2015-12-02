d17d86c7881febb99ade955691223d6a312dfa46
zubair alam <zubair.alam.jmi@gmail.com>
2015-12-02 17:26:13 +0000

    reactjs compiling added

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,10 @@ const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglifyjs');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
 
 var config = {
   bowerDir: './bower_components',
@@ -59,18 +63,6 @@ gulp.task('express', function () {
     app.listen(4000, '0.0.0.0');
 });
 
-// gulp.task('sass', function() {
-//   gulp.src('./public/stylesheets/scss/**/*.{scss,sass}')
-//     // Initializes sourcemaps
-//     .pipe(sourcemaps.init())
-//     .pipe(sass({
-//       errLogToConsole: true
-//       }))
-//     // Writes sourcemaps into the CSS file
-//     .pipe(sourcemaps.write())
-//     .pipe(gulp.dest('./public/stylesheets/css'));
-// })
-
 gulp.task('watch', function() {
   // Watches the scss folder for all .scss and .sass files
   // If any file changes, run the sass task
@@ -78,7 +70,15 @@ gulp.task('watch', function() {
   // gulp.watch(config.publicDir + '/javascripts/**/*.js', ['js']);
 })
 
-gulp.task('default', ['css', 'vendor_js', 'js', 'fonts', 'express', 'watch'], function () {
+gulp.task('build', function () {
+    return browserify({entries: './public/javascripts/app/app.js', extensions: ['.js'], debug: true})
+        .transform(babelify, {presets: ['es2015', 'react']})
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./public/javascripts/'));
+});
+
+gulp.task('default', ['css', 'vendor_js', 'build', 'fonts', 'express', 'watch'], function () {
 
 })
 
---
+++ public/javascripts/app/components/Counter.js
@@ -0,0 +1,20 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: props.initialCount};
+  }
+  tick() {
+    this.setState({count: this.state.count + 1});
+  }
+  render() {
+    return (
+      <div className="btn btn-default" onClick={this.tick.bind(this)}>
+        Clicks: {this.state.count}
+      </div>
+    );
+  }
+}
+Counter.propTypes = { initialCount: React.PropTypes.number };
+Counter.defaultProps = { initialCount: 0 };
